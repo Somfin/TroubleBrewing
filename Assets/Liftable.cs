94f161024f346c60a5794c6d91b7445ff1695c59
Somfin <micr0softener@gmail.com>
2016-01-30 11:12:03 +1300

    Pushing target-finding control will work.

---
+++ Assets/Liftable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Liftable : MonoBehaviour {
+    public LiftableType type;
+}
---
+++ Assets/Lifter.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lifter : MonoBehaviour {
+    public string liftCommand;
+    public GameObject holdPoint;
+    public LiftableType[] capabilities;
+
+    private GameObject liftedObject;
+    public GameObject targetObject;
+    private bool attemptingLift;
+    public bool lifting;
+
+	void Update () {
+        if (Input.GetAxis(liftCommand) > 0.1 && ! attemptingLift)
+        {
+            attemptingLift = true;
+            lifting = true;
+        }
+        else {
+            attemptingLift = false;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (lifting)
+        {
+            if (targetObject != null)
+            {
+                lifting = false;
+                targetObject = null;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        Liftable liftable = c.gameObject.GetComponent("Liftable") as Liftable;
+        if (liftable != null)
+        {
+            for (int i = 0; i < capabilities.Length; i++)
+            {
+                if (capabilities[i] == liftable.type)
+                {
+                    targetObject = c.gameObject;
+                }
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        Liftable liftable = c.gameObject.GetComponent("Liftable") as Liftable;
+        if (liftable != null)
+        {
+            if (targetObject == null || (c.gameObject.transform.position - this.gameObject.transform.position).magnitude < (this.targetObject.transform.position - this.gameObject.transform.position).magnitude)
+            {
+                this.targetObject = c.gameObject;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject == targetObject)
+        {
+            targetObject = null;
+        }
+    }
+}
