e35ecd993e9148e9e862cd7aeec8020b97102aa7
ScottBrebner <scotty.brebner@gmail.com>
2016-01-31 09:48:28 +1300

    Timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public float timer;
+    public Text timerText;
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer <= 11)
+        {
+            timerText.color = Color.red;
+        }
+        if(timer <= 0)
+        {
+            timer = 0;
+        }
+	}
+
+    void OnGUI()
+    {
+        timerText.text = (Mathf.Floor(timer / 60).ToString("0") + ":" + Mathf.Floor(timer % 60).ToString("00"));
+    }
+}
