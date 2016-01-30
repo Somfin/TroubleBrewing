5dc51a312df0f071dbfa531e462b515b6f33bb19
Somfin <micr0softener@gmail.com>
2016-01-31 11:46:56 +1300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Scott/Book.png.meta
    #	Assets/Scott/Book.psd.meta

---
+++ Assets/PositionTo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionTo : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Camera.main.WorldToScreenPoint(transform.parent.gameObject.transform.position);
+	}
+}
