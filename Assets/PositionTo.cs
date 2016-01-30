3d82249d75c3584f0d48042e199e2a681f7adbed
Somfin <micr0softener@gmail.com>
2016-01-31 11:56:38 +1300

    PositionTo works really nicely.

--- Assets/PositionTo.cs
+++ Assets/PositionTo.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 
 public class PositionTo : MonoBehaviour {
-	
+    private GameObject child;
+
+    void Start()
+    {
+        this.child = gameObject.transform.parent.gameObject;
+        gameObject.transform.SetParent(GameObject.Find("CameraCanvas").transform, false);
+    }
+
 	// Update is called once per frame
 	void Update () {
-        this.transform.position = Camera.main.WorldToScreenPoint(transform.parent.gameObject.transform.position);
+        this.transform.position = Camera.main.WorldToScreenPoint(child.transform.position);
 	}
 }
