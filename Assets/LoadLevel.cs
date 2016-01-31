35efd25ed20763a1c410df73ca98a22452dc1454
ScottBrebner <scotty.brebner@gmail.com>
2016-01-31 14:58:30 +1300

    Main Menu

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public void Load(string LevelToLoad)
+    {
+        Application.LoadLevel(LevelToLoad);
+    }
+
+}
